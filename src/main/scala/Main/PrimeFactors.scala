0cbd958598cd8d24ea80b885467d4aca13f45654
Ivett Ordog <devillmeister@gmail.com>
2020-04-30 12:37:56 +0200

    Prime factors

--- src/main/scala/Main/PrimeFactors.scala
+++ src/main/scala/Main/PrimeFactors.scala
@@ -2,8 +2,8 @@ package Main
 
 object PrimeFactors {
   def factorize(value: Int) : List[Int] = {
-    if (value % 3 == 0) factorize(value/3) ::: List(3) else
-    if (value % 2 == 0) factorize(value/2) ::: List(2) else
+    if (value % 3 == 0) factorize(value/3) :+ 3 else
+    if (value % 2 == 0) factorize(value/2) :+ 2 else
     if (value > 1) List(value) else List.empty
   }
 
